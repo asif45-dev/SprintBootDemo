87152804d783d22bde5aae72c19edada8fb7c706
Asif Pasha <asifm@emids.com>
2019-11-07 13:15:53 +0530

    "First commit to SprinBootDemo"

---
+++ src/test/java/com/emids/ehealth/SprintBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emids.ehealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
